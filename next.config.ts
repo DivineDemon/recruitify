364dc5720bb0bbcac0f05e199fa36690562dc61d
DivineDemon <supame123@gmail.com>
2025-11-09 22:42:41 +0500

    Boilerplate.

---
+++ next.config.ts
@@ -0,0 +1,6 @@
+import "./src/env.ts";
+
+/** @type {import("next").NextConfig} */
+const config = {};
+
+export default config;
---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { ModeToggle } from "@/components/mode-toggle";
+
+const Page = () => {
+	return (
+		<div className="flex h-screen w-full items-center justify-center">
+			<ModeToggle />
+		</div>
+	);
+};
+
+export default Page;
